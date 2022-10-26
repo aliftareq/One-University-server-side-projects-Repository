b68eb87325e001d60f862c9297d7563549b4295b
aliftareq <aliftareq@gmail.com>
2022-10-26 23:05:22 +0600

    added data

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+const courses = require('./Data/Courses.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('One University api running')
+})
+
+//sending all courses
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+//sending single course
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const selectedCourse = courses.find(n => n.id === id)
+    res.send(selectedCourse)
+})
+
+
+app.listen(port, () => {
+    console.log(`One university server running on port : ${port}`);
+})
